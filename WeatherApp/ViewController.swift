77bbdfc3aecc4ca1fdeda537605c5a1e15983ba4
Akindele Oluwakamiye <karmmie1@gmail.com>
2019-10-01 01:17:47 +0100

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Oluwakamiye Akindele on 01/10/2019.
+//  Copyright © 2019 Oluwakamiye Akindele. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
